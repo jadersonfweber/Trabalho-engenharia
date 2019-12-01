20b1dcdc59b162b2c6a1a7d41867e5473bfa40c8
Lojas Taqi <Lojas Taqi@TaQi>
2019-12-01 19:40:51 -0200

    Primeiro commit

---
+++ Trabalho engenharia/src/trabalho/engenharia/TrabalhoEngenharia.java
@@ -0,0 +1,13 @@
+
+package trabalho.engenharia;
+
+
+public class TrabalhoEngenharia {
+
+   
+    public static void main(String[] args) {
+
+        System.out.println("Trabalho engenharia");
+    }
+    
+}
